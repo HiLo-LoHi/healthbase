388a6857d609b5880f67dc797f00ed430bd17cf2
B.T. <briantanteo28@gmail.com>
2026-07-05 20:22:17 +0800

    Enhance Vaccination schema with JSON transformation
    Added JSON transformation for vaccination model.

--- backend/models/Vaccination.js
+++ backend/models/Vaccination.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 const vaccinationSchema = new mongoose.Schema({
-    residentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Resident', required: true },
-    vaccineType: { type: String, required: true },
-    doseNumber: String,
-    dateAdministered: Date
+  residentId:       { type: mongoose.Schema.Types.ObjectId, ref: 'Resident', required: true },
+  vaccineType:      { type: String, required: true },
+  doseNumber:       String,
+  dateAdministered: Date
 }, { timestamps: true });
+
+vaccinationSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    return ret;
+  }
+});
 
 module.exports = mongoose.model('Vaccination', vaccinationSchema);

5ae75d889955bef8c9a7c313e342add9943bb1f4
txdsim <simonmiguelcastillo05@gmail.com>
2026-07-05 11:15:46 +0800

    Add DNS fix for MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 const cors     = require('cors');
+
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 require('dotenv').config();
 
 const app = express();
